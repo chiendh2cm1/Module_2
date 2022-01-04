186f850ac87708de5027d7e00c71d342c35e0476
chiendh2cm1 <chiendh2cm1@gmail.com>
2022-01-04 10:41:22 +0700

    add dataType

---
+++ week_1/Leason_1/dataType/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        byte b = 12;
+        short c = 30;
+        long d = 32;
+        float e = 10.2f;
+        double f = 3.5;
+        char g = 'a';
+        String h = "Hà Nội";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
