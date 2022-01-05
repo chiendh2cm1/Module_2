5429ec443f0e2ed0745fefb0b2eb3309df2a8521
chiendh2cm1 <chiendh2cm1@gmail.com>
2022-01-05 21:28:15 +0700

    min in array

---
+++ week_1/Leason_3/min-in-array/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 12, 6, 8, 23, 9};
+        int index = minValue(arr);
+        System.out.println("The smallest element in the array is: " + arr[index]);
+    }
+
+    public static int minValue(int[] array) {
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < array[index]) {
+                index = i;
+            }
+        }
+        return index;
+    }
+}
