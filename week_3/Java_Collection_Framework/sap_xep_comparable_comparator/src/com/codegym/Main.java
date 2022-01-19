8feef07c68943b54def6f1dffef57c7b11c4754d
chiendh2cm1 <chiendh2cm1@gmail.com>
2022-01-19 11:18:45 +0700

    sap_xep_comparable_comparator

--- week_3/Java_Collection_Framework/sap_xep_comparable_comparator/src/com/codegym/Main.java
+++ week_3/Java_Collection_Framework/sap_xep_comparable_comparator/src/com/codegym/Main.java
@@ -1,27 +1,33 @@
 package com.codegym;
 
-import java.util.Scanner;
-import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("nhập chuỗi: ");
-        String name = sc.nextLine();
-        TreeMap<Character, Integer> treeMap = new TreeMap<>();
-        name = name.toLowerCase();
-        int value;
-        for (int i = 0; i < name.length(); i++) {
-            char key = name.charAt(i);
-            if (!treeMap.containsKey(key)){
-                treeMap.put(key, 1);
-            }else {
-                value = treeMap.get(key);
-                treeMap.remove(key);
-                treeMap.put(key,++value);
-            }
+        Student student = new Student("Kien", 30, "HT");
+        Student student1 = new Student("Nam", 26, "HN");
+        Student student2 = new Student("Anh", 38, "HT");
+        Student student3 = new Student("Tung", 38, "HT");
+
+        List<Student> lists = new ArrayList<>();
+        lists.add(student);
+        lists.add(student1);
+        lists.add(student2);
+        lists.add(student3);
+        Collections.sort(lists);
+        for (Student st : lists) {
+            System.out.println(st.toString());
         }
-        System.out.println(treeMap);
+
+        AgeComparator ageComparator = new AgeComparator();
+        Collections.sort(lists, ageComparator);
+        System.out.println("So sanh theo tuoi:");
+        for (Student st : lists) {
+            System.out.println(st.toString());
+
+        }
     }
 }
