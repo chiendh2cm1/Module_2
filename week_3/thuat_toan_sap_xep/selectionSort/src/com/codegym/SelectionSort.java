ce5b1de9dc8f3f36da9393ff0dc387cf7cb7c522
chiendh2cm1 <chiendh2cm1@gmail.com>
2022-01-20 17:25:06 +0700

    selectionSort

---
+++ week_3/thuat_toan_sap_xep/selectionSort/src/com/codegym/SelectionSort.java
@@ -0,0 +1,32 @@
+package com.codegym;
+
+public class SelectionSort {
+
+    static double[] list = {1, 9, 4.5, 6.6, 5.7, -4.5};
+
+    public static void selectionSort(double[] list) {
+        int N = list.length;
+        for (int i = 0; i < N; i++) {
+//            double Min = list[i];
+            int minIndex = i;
+            for (int j = i + 1; j < N; j++) {
+                if (list[minIndex] > list[j]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                double temp = list[i];
+                list[i] = list[minIndex];
+                list[minIndex] = temp;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        selectionSort(list);
+        for (double value: list){
+            System.out.print(value + "   ");
+        }
+    }
+}
