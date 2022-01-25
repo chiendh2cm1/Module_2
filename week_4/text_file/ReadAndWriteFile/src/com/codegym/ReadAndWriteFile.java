3021acf18633d0a4b5dd20a709d2300d5d8bb7fd
chiendh2cm1 <chiendh2cm1@gmail.com>
2022-01-25 11:15:33 +0700

    ReadAndWriteFile

---
+++ week_4/text_file/ReadAndWriteFile/src/com/codegym/ReadAndWriteFile.java
@@ -0,0 +1,39 @@
+package com.codegym;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadAndWriteFile {
+
+    public List<Integer> readFile(String filePath) {
+        List<Integer> numbers = new ArrayList<>();
+        try {
+            File file = new File(filePath);
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                numbers.add(Integer.parseInt(line));
+            }
+            bufferedReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return numbers;
+    }
+
+    public void writeFile(String filePath, int max) {
+        try {
+            FileWriter writer = new FileWriter(filePath, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+            bufferedWriter.write("Giá trị lớn nhất là: " + max);
+            bufferedWriter.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
